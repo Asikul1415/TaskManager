8a4cef916d15ecdcfa1be075c247c6380b57f5fa
Asikul1415 <asikulsteam@yandex.ru>
2026-08-19 20:36:00 +0500

    +feat(): примитивное выполнение задач

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
-package taskmanager
+package main
+
+import (
+	"log"
+)
 
 func main() {
+	type Task struct {
+		Id          int
+		Type        string
+		Description string
+	}
 
+	var queue []Task = []Task{
+		{Id: 0, Type: "http-request", Description: "path: https://google.com"},
+		{Id: 1, Type: "http-request", Description: "path: https://youtube.com"},
+		{Id: 2, Type: "http-request", Description: "path: https://wikipedia.com"},
+	}
+
+	for _, task := range queue {
+		log.Printf("Задача id %d была успешно выполнена", task.Id)
+	}
 }
